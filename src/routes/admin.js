b61d84fbdb667efa920ba04023982d491bd8e37e
ThinhLam <thinhlam240499@gmail.com>
2025-02-05 22:10:28 +0700

    ok

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -32,70 +32,71 @@ import AccountModel from "../components/account/AccountModel";
 import LoginAdmin from "../pages/loginAdmin";
 import Profile from "../pages/Profile";
 import ProfileTable from "../components/profile/ProfileModel";
-import OrderTable from '../components/order/OrderTable';
+import OrderTable from "../components/order/OrderTable";
 import Statistic from "../pages/Statistic";
+import ForgotPassword from "../components/forgotPassword";
 
 export const AdminRoutes = (
-    <>
-        <Route path="admin/login" element={<LoginAdmin />} />
-        <Route path="admin" element={<Dashboard />}>
-            <Route path="discount" element={<Discount />}>
-                <Route index element={<DiscountTable />} />
-                <Route path="create" element={<DiscountModel type="create" />} />
-                <Route path="edit/:id" element={<DiscountModel type="edit" />} />
-            </Route>
-            <Route path="voucher" element={<Voucher />}>
-                <Route index element={<VoucherTable />} />
-                <Route path="create" element={<VoucherModel type="create" />} />
-                <Route path="edit/:id" element={<VoucherModel type="edit" />} />
-            </Route>
-            <Route path="tshirt" element={<TshirtAdmin />}>
-                <Route index element={<TshirtTable />} />
-                <Route path="create" element={<TshirtModel type="create" />} />
-                <Route path="edit/:id" element={<TshirtModel type="edit" />} />
-            </Route>
-            <Route path="pant" element={<TshirtAdmin />}>
-                <Route index element={<PantTable />} />
-                <Route path="create" element={<PantModel type="create" />} />
-                <Route path="edit/:id" element={<PantModel type="edit" />} />
-            </Route>
-            <Route path="shoes" element={<ShoesAdmin />}>
-                <Route index element={<ShoesTable />} />
-                <Route path="create" element={<ShoesModel type="create" />} />
-                <Route path="edit/:id" element={<ShoesModel type="edit" />} />
-            </Route>
-            <Route path="accessory" element={<AccessoryAdmin />}>
-                <Route index element={<AccessoryTable />} />
-                <Route path="create" element={<AccessoryModel type="create" />} />
-                <Route path="edit/:id" element={<AccessoryModel type="edit" />} />
-            </Route>
-            <Route path="pantTshirtSize" element={<Size />}>
-                <Route index element={<PantShirtSizeTable />} />
-                <Route path="create" element={<PantShirtSizeModel type="create" />} />
-                <Route path="edit/:id" element={<PantShirtSizeModel type="edit" />} />
-            </Route>
-            <Route path="shoesSize" element={<Size />}>
-                <Route index element={<ShoesSizeTable />} />
-                <Route path="create" element={<ShoesSizeModel type="create" />} />
-                <Route path="edit/:id" element={<ShoesSizeModel type="edit" />} />
-            </Route>
-            <Route path="account" element={<Account />}>
-                <Route index element={<AccountTable />} />
-                <Route path="create" element={<AccountModel type="create" />} />
-                <Route path="edit/:id" element={<AccountModel type="edit" />} />
-            </Route>
-            <Route path="import" element={<Import />}>
-                <Route index element={<ImportTable />} />
-                <Route path="create" element={<ImportModel type="create" />} />
-                <Route path="edit/:id" element={<ImportModel type="edit" />} />
-            </Route>
-            <Route path='order' element={<Voucher />}>
-                <Route index element={<OrderTable />} />
-            </Route>
-            <Route path='statistic' element={<Statistic />}></Route>
-            <Route path="profile" element={<Profile />}>
-                <Route index element={<ProfileTable />} />
-            </Route>
-        </Route>
-    </>
+  <>
+    <Route path="admin/login" element={<LoginAdmin />} />
+    <Route path="admin" element={<Dashboard />}>
+      <Route path="discount" element={<Discount />}>
+        <Route index element={<DiscountTable />} />
+        <Route path="create" element={<DiscountModel type="create" />} />
+        <Route path="edit/:id" element={<DiscountModel type="edit" />} />
+      </Route>
+      <Route path="voucher" element={<Voucher />}>
+        <Route index element={<VoucherTable />} />
+        <Route path="create" element={<VoucherModel type="create" />} />
+        <Route path="edit/:id" element={<VoucherModel type="edit" />} />
+      </Route>
+      <Route path="tshirt" element={<TshirtAdmin />}>
+        <Route index element={<TshirtTable />} />
+        <Route path="create" element={<TshirtModel type="create" />} />
+        <Route path="edit/:id" element={<TshirtModel type="edit" />} />
+      </Route>
+      <Route path="pant" element={<TshirtAdmin />}>
+        <Route index element={<PantTable />} />
+        <Route path="create" element={<PantModel type="create" />} />
+        <Route path="edit/:id" element={<PantModel type="edit" />} />
+      </Route>
+      <Route path="shoes" element={<ShoesAdmin />}>
+        <Route index element={<ShoesTable />} />
+        <Route path="create" element={<ShoesModel type="create" />} />
+        <Route path="edit/:id" element={<ShoesModel type="edit" />} />
+      </Route>
+      <Route path="accessory" element={<AccessoryAdmin />}>
+        <Route index element={<AccessoryTable />} />
+        <Route path="create" element={<AccessoryModel type="create" />} />
+        <Route path="edit/:id" element={<AccessoryModel type="edit" />} />
+      </Route>
+      <Route path="pantTshirtSize" element={<Size />}>
+        <Route index element={<PantShirtSizeTable />} />
+        <Route path="create" element={<PantShirtSizeModel type="create" />} />
+        <Route path="edit/:id" element={<PantShirtSizeModel type="edit" />} />
+      </Route>
+      <Route path="shoesSize" element={<Size />}>
+        <Route index element={<ShoesSizeTable />} />
+        <Route path="create" element={<ShoesSizeModel type="create" />} />
+        <Route path="edit/:id" element={<ShoesSizeModel type="edit" />} />
+      </Route>
+      <Route path="account" element={<Account />}>
+        <Route index element={<AccountTable />} />
+        <Route path="create" element={<AccountModel type="create" />} />
+        <Route path="edit/:id" element={<AccountModel type="edit" />} />
+      </Route>
+      <Route path="import" element={<Import />}>
+        <Route index element={<ImportTable />} />
+        <Route path="create" element={<ImportModel type="create" />} />
+        <Route path="edit/:id" element={<ImportModel type="edit" />} />
+      </Route>
+      <Route path="order" element={<Voucher />}>
+        <Route index element={<OrderTable />} />
+      </Route>
+      <Route path="statistic" element={<Statistic />}></Route>
+      <Route path="profile" element={<Profile />}>
+        <Route index element={<ProfileTable />} />
+      </Route>
+    </Route>
+  </>
 );
