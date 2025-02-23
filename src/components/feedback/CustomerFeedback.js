5226918465c63aac77e2cc1ef6bc0d10c3bfac8a
ThinhLam <thinhlam240499@gmail.com>
2025-02-23 17:22:52 +0700

    first push

--- src/components/feedback/CustomerFeedback.js
+++ src/components/feedback/CustomerFeedback.js
@@ -1,135 +1,272 @@
-import { Button, Col, Form, List, Modal, Row, message } from 'antd';
-import TextArea from 'antd/es/input/TextArea';
-import React, { useEffect, useState } from 'react';
-import '../../assets/css/feedback.css';
-import { createFeedback, getFeedBack, getListFeedback, updateFeedback } from '../../services/feedback.service';
-import { API_PATH } from '../../config/api.config';
-import { showDeleteConfirm } from '../../utils/helper';
+import { LikeFilled, LikeOutlined, MoreOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Col,
+  Dropdown,
+  Form,
+  List,
+  Menu,
+  Modal,
+  Row,
+  message,
+} from "antd";
+import TextArea from "antd/es/input/TextArea";
+import React, { useEffect, useState } from "react";
+import "../../assets/css/feedback.css";
+import { API_PATH } from "../../config/api.config";
+import {
+  createFeedback,
+  getFeedbackLike,
+  getListFeedback,
+  likeFeedback,
+  updateFeedback,
+} from "../../services/feedback.service";
+import { showDeleteConfirm } from "../../utils/helper";
+import { useAuth } from "../context/AuthContext";
 
-const CustomerFeedback = ({ accessory_id, pant_id, tshirt_id, shoes_id, userId }) => {
-    const [feedback, setFeedback] = useState([]);
-    const [form] = Form.useForm();
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [messageApi, contextHolder] = message.useMessage()
-    const [currentFeedback, setCurrentFeedback] = useState(false);
-    const [id, setId] = useState('');
+const CustomerFeedback = ({ product_id, userId, feedbackId }) => {
+  const [feedback, setFeedback] = useState([]);
+  const [feedbackArr, setFeedbackArr] = useState([]);
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [messageApi, contextHolder] = message.useMessage();
+  const [currentFeedback, setCurrentFeedback] = useState(false);
+  const [id, setId] = useState("");
 
-    const selectId = accessory_id || pant_id || tshirt_id || shoes_id
-    const onFinish = (values) => {
-        console.log('Success:', values.content);
-        console.log(userId);
-        const feedbackData = {
-            content: values.content,
-            account_id: userId,
-            accessory_id: accessory_id,
-            pant_id: pant_id,
-            tshirt_id: tshirt_id,
-            shoes_id: shoes_id
-        }
-        if (currentFeedback) {
-            updateFeedback(id, feedbackData)
-        } else {
-            createFeedback(feedbackData)
-        }
-        setIsModalOpen(false)
+  const { user } = useAuth();
+  const [refreshTrigger, setRefreshTrigger] = useState(0);
+  const selectId = product_id;
+  const onFinish = (values) => {
+    console.log("Success:", values.content);
+    console.log(userId);
+    const feedbackData = {
+      content: values.content,
+      account_id: userId,
+      product_id,
     };
-    const editFeedback = (feedbackItem, id) => {
-        setCurrentFeedback(true);
-        setId(id)
-        console.log(id);
-        form.setFieldsValue({ content: feedbackItem.content });
-        setIsModalOpen(true);
-    };
+    if (currentFeedback) {
+      updateFeedback(id, feedbackData);
+      setCurrentFeedback(false);
+    } else {
+      createFeedback(feedbackData);
+    }
+    form.resetFields();
+    setIsModalOpen(false);
+    setRefreshTrigger((prev) => prev + 1);
+  };
+  const editFeedback = (feedbackItem, id) => {
+    setCurrentFeedback(true);
+    setId(id);
+    console.log(id);
+    form.setFieldsValue({ content: feedbackItem.content });
+    setIsModalOpen(true);
+  };
 
-    const showModal = () => {
-        setIsModalOpen(true);
-    };
-    // const handleOk = () => {
-    //     setIsModalOpen(false);
-    // };
-    const handleCancel = () => {
-        setIsModalOpen(false);
-    };
-    useEffect(() => {
-        getListFeedback(selectId, setFeedback)
-    }, [feedback, isModalOpen, selectId])
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
 
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
-    return (
-        <div style={{ width: 900, margin: 'auto', marginTop: 100 }}>
-            <Row style={{ justifyContent: 'space-between', paddingBottom: 40, }}>
-                <p style={{ fontSize: 25, fontWeight: 'bold' }}>Feedback</p>
-                <Button style={{ backgroundColor: "black", borderRadius: 0 }} type="primary" onClick={showModal}>
-                    Write Feedback
-                </Button>
-            </Row>
-            <Modal title="Write new feedback" open={isModalOpen} onCancel={handleCancel} footer={null}>
-                <Form
-                    className='customer-feedback'
-                    name="basic"
-                    form={form}
-                    initialValues={{
-                        remember: true,
-                    }}
-                    onFinish={onFinish}
-                >
-                    <Form.Item
-                        name='content'
-                        rules={[
-                            {
-                                min: 3,
-                                max: 200,
-                                message: 'Content length must be between 3 and 200 characters.',
-                            },
-                        ]}
-                    >
-                        <TextArea autoSize={{
-                            minRows: 4,
-                            maxRows: 5,
-                        }} />
-                    </Form.Item>
+  const handleMenuClick = (item, id) => (e) => {
+    if (e.key === "edit") {
+      editFeedback(item, id);
+    } else if (e.key === "delete") {
+      showDeleteConfirm(
+        id,
+        messageApi,
+        () => getListFeedback(selectId, setFeedback),
+        setFeedback,
+        API_PATH.feedback
+      );
+    }
+  };
+  useEffect(() => {
+    if (selectId) {
+      getListFeedback(selectId, setFeedback);
+    }
+    if (user.id) {
+      getFeedbackLike(user.id).then((data) => {
+        setFeedbackArr(data);
+      });
+    }
+  }, [selectId, user?.id, refreshTrigger]);
 
-                    <Form.Item style={{ marginTop: 20 }}>
-                        <Button color="default" variant="solid" htmlType="submit" style={{ marginRight: 15 }}>
-                            Send feedback
-                        </Button>
-                        <Button color="default" variant="outlined" onClick={() => form.resetFields()}>
-                            Cancel
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Modal>
-
-
-            <List
-                style={{ padding: '30px 0', borderTop: '1px solid gray', borderBottom: '1px solid gray' }}
-                itemLayout="horizontal"
-                dataSource={feedback}
-                split={false}
-                renderItem={(item, index) => (
-                    <List.Item style={{ marginBottom: 15, display: "block", }}>
-                        <Row>
-                            <Col span={6}>
-                                <b>{item.account_id.username}</b>
-                                <p>{new Date(item.createdAt).toLocaleDateString()}</p>
-                            </Col>
-                            <Col span={18}>
-                                {item.content}
-                                <Row style={{ marginTop: 10 }}>
-                                    {userId === item.account_id._id && (
-                                        <>
-                                            <Button style={{ backgroundColor: '#FF9800', color: 'white', marginRight: 10 }} onClick={() => editFeedback(item, item._id)}>Edit</Button>
-                                            <Button style={{ backgroundColor: '#FF5252  ', color: 'white' }} onClick={() => showDeleteConfirm(item._id, messageApi, () => getListFeedback(selectId, setFeedback), setFeedback, API_PATH.feedback)}>Remove</Button>
-                                        </>
-                                    )}
-                                </Row>
-                            </Col>
-                        </Row>
-                    </List.Item>
-                )}
-            />
-        </div>
+  useEffect(() => {
+    console.log("Feedback array đã được cập nhật:", feedbackArr);
+  }, [feedbackArr]);
+  const likeButton = async (feedback_id, account_id) => {
+    setFeedbackArr((prev) =>
+      prev.includes(feedback_id)
+        ? prev.filter((id) => id !== feedback_id)
+        : [...prev, feedback_id]
     );
+    try {
+      await likeFeedback(feedback_id, account_id);
+      const updatedLikes = await getFeedbackLike(account_id);
+      setFeedbackArr(updatedLikes);
+    } catch (error) {
+      console.error("Lỗi khi cập nhật like:", error);
+    }
+
+    setRefreshTrigger((prev) => prev + 1);
+  };
+
+  return (
+    <div style={{ width: 900, margin: "auto", marginTop: 100 }}>
+      <Row style={{ justifyContent: "space-between", paddingBottom: 40 }}>
+        <p style={{ fontSize: 25, fontWeight: "bold" }}>Feedback</p>
+        <Button
+          style={{ backgroundColor: "black", borderRadius: 0 }}
+          type="primary"
+          onClick={showModal}
+        >
+          Write Feedback
+        </Button>
+      </Row>
+      <Modal
+        title="Write new feedback"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <Form
+          className="customer-feedback"
+          name="basic"
+          form={form}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="content"
+            rules={[
+              {
+                min: 3,
+                max: 200,
+                message: "Content length must be between 3 and 200 characters.",
+              },
+            ]}
+          >
+            <TextArea
+              autoSize={{
+                minRows: 4,
+                maxRows: 5,
+              }}
+            />
+          </Form.Item>
+
+          <Form.Item style={{ marginTop: 20 }}>
+            <Button
+              color="default"
+              variant="solid"
+              htmlType="submit"
+              style={{ marginRight: 15 }}
+            >
+              Send feedback
+            </Button>
+            <Button
+              color="default"
+              variant="outlined"
+              onClick={() => form.resetFields()}
+            >
+              Cancel
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+
+      <List
+        style={{
+          padding: "30px 0",
+          borderTop: "1px solid gray",
+          borderBottom: "1px solid gray",
+        }}
+        itemLayout="horizontal"
+        dataSource={feedback}
+        split={false}
+        renderItem={(item, index) => {
+          const menu = (
+            <Menu onClick={handleMenuClick(item, item._id)}>
+              <Menu.Item key="edit">Edit</Menu.Item>
+              <Menu.Item key="delete">Remove</Menu.Item>
+            </Menu>
+          );
+          return (
+            <List.Item style={{ marginBottom: 15, display: "block" }}>
+              <Row style={{ display: "flex", alignItems: "center" }}>
+                <Col span={6}>
+                  <b>{item.account_id.username}</b>
+                </Col>
+                <Col span={4} offset={13}>
+                  <p>{new Date(item.createdAt).toDateString()}</p>
+                </Col>
+                {userId === item.account_id._id && (
+                  <Col span={1}>
+                    <Dropdown overlay={menu} trigger={["click"]}>
+                      <Button
+                        style={{ borderColor: "white" }}
+                        icon={<MoreOutlined />}
+                      />
+                    </Dropdown>
+                  </Col>
+                )}
+              </Row>
+              <div
+                style={{
+                  marginLeft: 20,
+                  backgroundColor: "#F3F4F6",
+                  height: 70,
+                  marginTop: 10,
+                  padding: 15,
+                  paddingRight: 0,
+                }}
+              >
+                <Row>
+                  <Col span={2}>
+                    <p>
+                      <b>Đánh giá: </b>
+                    </p>
+                  </Col>
+                  {/* <Col>
+                    <Rate allowHalf disabled defaultValue={2.5} />
+                  </Col> */}
+                </Row>
+                <Row>
+                  <Col span={2}>
+                    <p>
+                      <b>Nhận xét: </b>
+                    </p>
+                  </Col>
+                  <Col>
+                    <p>{item.content}</p>
+                  </Col>
+                </Row>
+              </div>
+
+              <Row>
+                <Button
+                  style={{ borderColor: "white" }}
+                  icon={
+                    feedbackArr.includes(item._id) ? (
+                      <LikeFilled />
+                    ) : (
+                      <LikeOutlined />
+                    )
+                  }
+                  onClick={() => likeButton(item._id, user.id)}
+                />
+                {item.likeCount}
+              </Row>
+            </List.Item>
+          );
+        }}
+      />
+    </div>
+  );
 };
 
 export default CustomerFeedback;
--- src/services/feedback.service.js
+++ src/services/feedback.service.js
@@ -1,38 +1,64 @@
-import axios from "axios"
-import { API_PATH } from "../config/api.config"
+import axios from "axios";
+import { API_PATH } from "../config/api.config";
 
 export const getListFeedback = (id, setFeedback) => {
-    axios.get(API_PATH.feedback + `/${id}`)
-        .then((res) => {
-            setFeedback(res.data)
-        })
-        .catch(error => console.error(error))
-}
+  axios
+    .get(API_PATH.feedback + `/${id}`)
+    .then((res) => {
+      setFeedback(res.data);
+      // console.log(res.data);
+    })
+    .catch((error) => console.error(error));
+};
 
 export const getFeedBack = () => {
-    axios.get(API_PATH.feedback)
-        .then((res) => {
-        })
-        .catch(error => console.error(error))
-}
-
+  axios
+    .get(API_PATH.feedback)
+    .then((res) => {})
+    .catch((error) => console.error(error));
+};
 
 export const createFeedback = (discount) => {
-    axios.post(API_PATH.feedback, discount)
-        .then((res) => {
-            console.log(res);
-        })
-        .catch(error => {
-            console.log(error)
-        })
-}
+  axios
+    .post(API_PATH.feedback, discount)
+    .then((res) => {
+      // console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 export const updateFeedback = (id, discount) => {
-    axios.put(API_PATH.feedback + `/${id}`, discount)
-        .then((res) => {
-            console.log(res);
-        })
-        .catch(error => {
-            console.log(error)
-        })
-}
+  axios
+    .put(API_PATH.feedback + `/${id}`, discount)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const likeFeedback = (id, account_id) => {
+  axios
+    .put(API_PATH.feedback + `/like/${id}`, { account_id })
+    .then((res) => {
+      return res.data.liked;
+      // console.log(res.data.liked);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const getFeedbackLike = (account_id) => {
+  return axios
+    .get(API_PATH.feedback + `/like/${account_id}`)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
